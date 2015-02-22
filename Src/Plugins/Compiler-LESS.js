851cc9ffcd7f58c452e33e29f8e629a5c2977603
steelbrain <aneesiqbalbhatti@gmail.com>
2015-02-22 14:50:15 -0800

    :construction: Upgrade Compiler-LESS to use CompilerBase

--- Src/Plugins/Compiler-LESS.js
+++ Src/Plugins/Compiler-LESS.js
@@ -3,158 +3,86 @@
 // @Compiler-Output "../../Built/Plugins/Compiler-LESS.js"
 var
   Promise = require('a-promise'),
+  UglifyCSS = null,
   LESS = null,
-  FS = require('fs'),
-  Path = require('path'),
   H = require('../H'),
-  Compiler = null;
-class CompilerLESS{
-  static RegexAppend:RegExp = /@(codekit-append|prepros-append|Compiler-Append)/;
-  static RegexOutput:RegExp = /@Compiler-Output/;
-  static RegexSourceMap:RegExp = /@Compiler-Sourcemap/;
-  static RegexCompress:RegExp = /@Compiler-Compress/;
-  static RegexExtract:RegExp = /".*"/;
-  static init(LeCompiler):void{
-    Compiler = LeCompiler;
-  }
-
-  static ExtractValue(Line:String):Promise{
-    return new Promise(function(resolve,reject){
-      var Result = CompilerLESS.RegexExtract.exec(Line);
-      if (!Result.length) {
-        return reject(); // Skip empty "@codekit-append"
+  Path = require('path'),
+  Compiler = null,
+  CompilerBase = require('../Abstract/Compiler-Base').CompilerBase;
+class CompilerLESS extends CompilerBase{
+  Map:Object = {
+    Comments: ['//','/*'],
+    Tags:{
+      'Compiler-Output':function(Info,Opts,Content,Line,Index,FileDir):void{
+        Opts.TargetFile = H.ABSPath(Info[2],FileDir);
+      },
+      'Compiler-Compress':function(Info,Opts):void{
+        Opts.Compress = Info[2] === 'true';
+      },
+      'Compiler-SourceMap': function(Info,Opts,Content,Line,Index,FileDir):void{
+        Opts.SourceMap = H.ABSPath(Info[2],FileDir);
+      },
+      'Compiler-Append':function(Info,Opts,Content,Line,Index,FileDir):Promise{
+        return new Promise(function(Resolve,Reject){
+          Compiler.Compile(H.ABSPath(Info[2],FileDir)).then(function(Result){
+            Resolve(Result.Content);
+          },Reject);
+        });
       }
-      Result = Result[0].substr(1, Result[0].length - 2);
-      resolve(Result);
-    });
+    }
+  };
+  constructor(CompilerInst){
+    Compiler = CompilerInst;
   }
-  static ExtractPath(Line:String, FileDir:String):Promise{
-    return new Promise(function(resolve,reject){
-      CompilerLESS.ExtractValue(Line).then(function(Result:String){
-        if (Result.substr(0, 1) !== Path.sep &&
-          Result.substr(1, 1) !== ':') { // Windows Drive `D:\`
-          Result = FileDir + Path.sep + Result;
-        }
-        resolve(Result);
-      },reject);
-    });
-  }
-
-  static ParseAppend(Line:String, FileDir: String, FilePath:String,HasSourceMap:Boolean):Promise{
-    return new Promise(function(resolve,reject){
-      CompilerLESS.ExtractPath(Line, FileDir).then(function(Result){
-        if(!HasSourceMap){
-          // Lets append it if we aren't giving em any source maps, EH!
-          Compiler.Compile(Result).then(function(Result){
-            resolve(Result.Content);
-          },reject)
-        } else {
-          FS.readFile(Result, function (Error, LeContent) {
-            if (Error) {
-              return reject(`The File '${Result} doesn't exist, It was imported in ${FilePath}'`);
+  Process(FilePath:String, Opts:Object):Promise{
+    return new Promise(function(Resolve,Reject){
+      H.ReadFile(FilePath).then(function(Content){
+        this.Parse(FilePath,Content,Opts).then(function(Parsed){
+          var ToReturn = {Content: Parsed.Content, SourceMap: '', Opts: Parsed.Opts};
+          this.ProcessLESS(FilePath, ToReturn, Parsed).then(function(){
+            if(Opts.Compress){
+              this.ProcessUglify(FilePath, ToReturn, Parsed);
             }
-            resolve(LeContent.toString());
-          });
-        }
-      },reject);
-    });
+            Resolve(ToReturn);
+          }.bind(this));
+        }.bind(this),Reject);
+      }.bind(this),Reject);
+    }.bind(this));
   }
-  static Parse(FilePath:String, Content:Array, Opts:Object):Promise{
-    return new Promise(function(resolve,reject){
+  ProcessLESS(FilePath, ToReturn, {Opts,Content}):Promise{
+    return new Promise(function(Resolve,Reject){
       var
-        ToReturn = {Content: "", Opts: Opts},
-        FileDir = Path.dirname(FilePath),
-        Promises = [];
-      Content.forEach(function(Line:String, LeIndex:Number){
-        var Index;
-        if(Line.indexOf('//') !== -1){
-          Promises.push(new Promise(function(LineResolve,LineReject){
-            Line = Line.trim();
-            Index = Line.indexOf('//');
-            if(Index === -1 || Index !== 0){
-              return LineResolve(); // Ignore non-commented lines or lines with stuff + comments
-            }
-            if(CompilerLESS.RegexAppend.test(Line)) {
-              CompilerLESS.ParseAppend(Line, FileDir,FilePath,!!Opts.SourceMap).then(function(Result){
-                Content[LeIndex] = Result;
-                LineResolve();
-              },LineReject);
-            } else if(CompilerLESS.RegexOutput.test(Line)) {
-              CompilerLESS.ExtractPath(Line, FileDir).then(function(Result) {
-                Opts.TargetFile = Result;
-                Content[LeIndex] = '';
-                LineResolve();
-              }, LineReject);
-            } else if(CompilerLESS.RegexSourceMap.test(Line)) {
-              CompilerLESS.ExtractPath(Line, FileDir).then(function (Result) {
-                Content[LeIndex] = '';
-                if (Result === '') {
-                  Opts.SourceMap = null;
-                } else {
-                  Opts.SourceMap = Result;
-                }
-                LineResolve();
-              });
-            } else if(CompilerLESS.RegexCompress.test(Line)){
-              CompilerLESS.ExtractPath(Line, FileDir).then(function (Result) {
-                Content[LeIndex] = '';
-                Opts.Compress = Result === 'true';
-                LineResolve();
-              });
-            } else {
-              LineResolve();
-            }
-          }));
+        Temp = null,
+        SourceMapURL = Opts.SourceMap !== null ? H.Relative(Path.dirname(Opts.TargetFile), Opts.SourceMap) : null,
+        FileDir = Path.dirname(FilePath);
+      LESS = LESS || require('less');
+      LESS.render(Content,{
+        sourceMap: true,
+        filename: FilePath,
+        paths: [FileDir],
+        compress: Opts.Compress
+      }).then(function(LeResult){
+        ToReturn.Content = LeResult.css;
+        if(Opts.SourceMap !== null){
+          Temp = {Map: JSON.parse(LeResult.map),Files:[], SourceMapDir: Opts.SourceMap.split(Path.sep)};
+          Temp.SourceMapDir.pop();
+          Temp.SourceMapDir = Temp.SourceMapDir.join(Path.sep);
+          Temp.Map.sources.forEach(function(File){
+            Temp.Files.push(H.Relative(Temp.SourceMapDir,File));
+          });
+          Temp.Map.sources = Temp.Files;
+          ToReturn.SourceMap = JSON.stringify(Temp.Map);
+          ToReturn.Content += '/*# sourceMappingURL=' + SourceMapURL + ' */';
         }
-      });
-      Promise.all(Promises).then(function(){
-        ToReturn.Content = Content.join("\n");
-        resolve(ToReturn);
-      },reject);
+        Resolve();
+      },Reject);
     });
   }
-  static Process(FilePath:String, Opts:Object):Promise{
-    return new Promise(function(resolve,reject){
-      FS.readFile(FilePath,function(Error,Content){
-        if(Error){
-          return reject(Error);
-        }
-        CompilerLESS.Parse(FilePath,Content.toString().split("\n"),Opts).then(function(Result){
-          Opts = Result.Opts;
-          var
-            HasSourceMap = Opts.SourceMap !== null,
-            ToReturn = {
-              Content: '',
-              SourceMap: '',
-              Opts: Opts
-            },
-            SourceMapURL = HasSourceMap ? H.Relative(Path.dirname(Opts.TargetFile), Opts.SourceMap) : null,
-            FileDir = Path.dirname(FilePath),
-            Temp = null;
-          LESS = LESS || require('less');
-          LESS.render(Result.Content,{
-            sourceMap: true,
-            filename: FilePath,
-            paths: [FileDir],
-            compress: Opts.Compress
-          }).then(function(LeResult){
-            ToReturn.Content = LeResult.css;
-            if(HasSourceMap){
-              Temp = {Map: JSON.parse(LeResult.map),Files:[], SourceMapDir: Opts.SourceMap.split(Path.sep)};
-              Temp.SourceMapDir.pop();
-              Temp.SourceMapDir = Temp.SourceMapDir.join(Path.sep);
-              Temp.Map.sources.forEach(function(File){
-                Temp.Files.push(H.Relative(Temp.SourceMapDir,File));
-              });
-              Temp.Map.sources = Temp.Files;
-              ToReturn.SourceMap = JSON.stringify(Temp.Map);
-              ToReturn.Content += '/*# sourceMappingURL=' + SourceMapURL + ' */';
-            }
-            resolve(ToReturn);
-          },reject);
-        },reject);
-      })
-    });
+  ProcessUglify(FilePath, ToReturn, {Opts,Content}){
+    UglifyCSS = UglifyCSS || new(require('clean-css'))({sourceMap:true});
+    var Output = UglifyCSS.minify(Content);
+    ToReturn.Content = Output.styles;
+    ToReturn.SourceMap = '';
   }
 }
 module.exports = CompilerLESS;
