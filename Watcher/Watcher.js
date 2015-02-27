8f4a0c0316942fcbe676cb2c94ea4f4ec36bf931
steelbrain <aneesiqbalbhatti@gmail.com>
2015-02-26 22:29:07 -0800

    :construction: Update Watcher Default Options

--- Watcher/Watcher.js
+++ Watcher/Watcher.js
@@ -8,7 +8,8 @@ var
   CompilerH = require('../Compiler/H'),
   {EventEmitter} = require('events'),
   Path = require('path'),
-  FS = require('fs');
+  FS = require('fs'),
+  Chokidar = require('chokidar');
 global.uc_watcher_debug = require('debug')('uc-watcher');
 class Watcher extends EventEmitter{
   Dir:String;
@@ -16,7 +17,7 @@ class Watcher extends EventEmitter{
   ManifestPath:String;
   constructor(Dir:String){
     this.Dir = FS.realpathSync(Dir);
-    this.ManifestPath = this.Dir + Path.sep + 'DeProc.json';
+    this.ManifestPath = `${this.Dir}${Path.sep}DeProc.json`;
     CompilerH.FileExists(this.ManifestPath).then(function(){
       CompilerH.FileRead(this.ManifestPath).then(function(Contents){
         this.Manifest = JSON.parse(Contents);
@@ -29,23 +30,27 @@ class Watcher extends EventEmitter{
         this.emit('Init');
       }.bind(this));
     }.bind(this));
+    this.on('Init',this.Watch.bind(this));
+  }
+  Watch(){
+
   }
 }
 class WatcherControl{
   static FileTypes:Object = {
-    'JS': {Compress: false, Compiler: ['Babel','ReactTools','Riot'], SourceMap: null},
-    'JSX': {Compress: false, Compiler: ['Babel','ReactTools','Riot'], SourceMap: null},
-    'TAG': {Compress: false, Compiler: ['Riot'], SourceMap: null},
-    'COFFEE': {Compress: false, SourceMap: null},
-    'LESS': {Compress: false, SourceMap: null},
-    'CSS': {Compress: false, SourceMap: null}
+    'JS': {Compress: false, Compiler: 'Babel', SourceMap: null, Output: null, IncludedIn:[]},
+    'JSX': {Compress: false, Compiler: 'Babel', SourceMap: null, Output: null, IncludedIn:[]},
+    'TAG': {Compress: false, Compiler: 'Babel', SourceMap: null, Output: null, IncludedIn:[]},
+    'COFFEE': {Compress: false, SourceMap: null, Output: null, IncludedIn:[]},
+    'LESS': {Compress: false, SourceMap: null, Output: null, IncludedIn:[]},
+    'CSS': {Compress: false, SourceMap: null, Output: null, IncludedIn:[]}
   };
   static Watch(Dir:String){
     if(CompilerH.FileExists(Dir)){
       global.uc_watcher_debug("WatcherControl::Watch Initiating new Watcher");
       return new Watcher(Dir);
     } else {
-      global.uc_watcher_debug("WatcherControl::Watch Doesn't exist " + Dir);
+      global.uc_watcher_debug(`WatcherControl::Watch Not Found ${Dir}`);
       throw new Error("The Path provided Doesn't exist");
     }
   }
