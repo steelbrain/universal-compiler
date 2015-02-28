01d7aa9e304d46e3b29213e2bce517906a57608b
steelbrain <aneesiqbalbhatti@gmail.com>
2015-02-28 13:45:45 -0800

    :arrow_up: Upgrade Exec/Exec to use Latest Compiler API

--- Exec/Exec.js
+++ Exec/Exec.js
@@ -1,12 +1,12 @@
 #!/usr/local/bin/babel-node -g
 
 var
-  {Compiler} = require('../Built/Compiler/Compiler');
+  Compiler = require('../Built/Compiler/Compiler').Compiler;
 if(!process.argv[2]){
   console.log("Please Specify a source File");
   process.exit(1);
 }
-Compiler.Compile(process.argv[2],process.argv[3],process.argv[4]).then(function(Result){
+Compiler.Compile(process.argv[2],{Write:true}).then(function(Result){
   if(!process.argv[3] && Result){
     process.stdout.write(Result.Content);
   }
