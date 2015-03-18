97452cc95d94b32251d8d75acb5f3a7fd6c79698
steelbrain <aneesiqbalbhatti@gmail.com>
2015-03-18 07:34:20 -0700

    :fire: Remove Old Main file

--- Main.js
+++
@@ -1,7 +0,0 @@
-var Compiler = require('./Built/Compiler/Compiler').Compiler;
-var WatcherPack = require('./Built/Watcher/Watcher');
-module.exports = {
-  Compiler: Compiler,
-  Watcher: WatcherPack.Watcher,
-  WatcherControl: WatcherPack.WatcherControl
-};
