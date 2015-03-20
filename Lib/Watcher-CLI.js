4ccb2b93de2f82012c2f5acf3454e5a9ec574f73
steelbrain <aneesiqbalbhatti@gmail.com>
2015-03-20 02:03:15 -0700

    :new: Add a working Watcher-CLI

---
+++ Lib/Watcher-CLI.js
@@ -0,0 +1,35 @@
+#!/usr/local/bin/iojs --es_staging
+
+
+"use strict";
+var
+  Watcher = require('./Watcher/Index'),
+  Opts = require('minimist')(process.argv.slice(2)),
+  Log = require('debug'),
+  FS = require('fs');
+Log.enable('uc-watcher-cli');
+Log = Log('uc-watcher-cli');
+class CLI{
+  static Init(){
+    if(!Opts['_'].length){
+      throw new Error("Please specify a directory to watch");
+    }
+    Opts.Watch = true;
+    try {
+      var Stats = FS.statSync(Opts['_'][0]);
+    } catch(err){
+      throw new Error(`The directory '${Opts['_'][0]}' doesn't exist`);
+    }
+    if(!Stats.isDirectory()){
+      throw new Error(`The input '${Opts['_'][0]}' is not a directory`);
+    }
+    UniversalCompiler.H.NormalizeOpts(Opts);
+    Watcher.Watch(Opts['_'][0], function(){});
+  }
+}
+
+try {
+  CLI.Init();
+} catch(error){
+  Log(error.message);
+}
