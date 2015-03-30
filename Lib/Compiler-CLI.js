0d1906e42ce29a8b08022c87dc471ef6e5aa824e
steelbrain <aneesiqbalbhatti@gmail.com>
2015-03-30 02:22:20 -0700

    :art: Inline the Compiler and Watcher Init

--- Lib/Compiler-CLI.js
+++ Lib/Compiler-CLI.js
@@ -6,12 +6,36 @@ var
   Compiler = require('../Compiler'),
   Opts = require('minimist')(process.argv.slice(2)),
   Log = require('debug'),
-  FS = require('fs'),
-  CLI = require('./CLI/Compiler')(Opts, FS, Log, Compiler);
+  FS = require('fs');
 Log.enable('uc-compiler-cli');
 Log = Log('uc-compiler-cli');
 try {
-  CLI.Init();
+  if(!Opts['_'].length){
+    throw new Error("Please specify a file to compile");
+  }
+  Opts.Write = true;
+  UniversalCompiler.H.NormalizeOpts(Opts);
+  Compiler.Compile(Opts['_'][0], Opts).then(function(FileInfo){
+    if(FileInfo.Opts.Output && FileInfo.Opts.Write){
+      try {
+        FS.writeFileSync(FileInfo.Opts.Output, FileInfo.Result);
+      } catch(error){
+        return Log(`Permission denied, can't write output to file '${FileInfo.Opts.Output}'`);
+      }
+      if(FileInfo.Opts.SourceMap){
+        try {
+          FS.writeFileSync(FileInfo.Opts.SourceMap, FileInfo.SourceMap);
+        } catch(error){
+          return Log(`Permission denied, can't write sourcemap to file '${FileInfo.Opts.SourceMap}'`);
+        }
+      }
+    } else {
+      console.log(FileInfo.Result);
+    }
+  }, function(error){
+    //Log(error.message);
+    Log(error.stack);
+  });
 } catch(error){
   Log(error.message);
 }
--- Lib/Watcher-CLI.js
+++ Lib/Watcher-CLI.js
@@ -6,13 +6,26 @@ var
   Watcher = require('../Watcher'),
   Opts = require('minimist')(process.argv.slice(2)),
   Log = require('debug'),
-  FS = require('fs'),
-  CLI = require('./CLI/Watcher')(Opts, Watcher, FS, Log);
+  FS = require('fs');
 Log.enable('uc-watcher-cli');
 Log = Log('uc-watcher-cli');
 
 try {
-  CLI.Init();
+  if(!Opts['_'].length){
+    throw new Error("Please specify a directory to watch");
+  }
+  Opts.Watch = true;
+  Opts.Excluded = Opts.Excluded ? Opts.Excluded.split(',') : [];
+  try {
+    var Stats = FS.statSync(Opts['_'][0]);
+  } catch(err){
+    throw new Error(`The directory '${Opts['_'][0]}' doesn't exist`);
+  }
+  if(!Stats.isDirectory()){
+    throw new Error(`The input '${Opts['_'][0]}' is not a directory`);
+  }
+  UniversalCompiler.H.NormalizeOpts(Opts);
+  Watcher.Watch(Opts['_'][0], function(){}, Opts.Excluded);
 } catch(error){
   Log(error.message);
 }
