49a86251ba0972472265212c5dff042b0072c2ff
steelbrain <aneesiqbalbhatti@gmail.com>
2015-06-14 14:42:02 -0600

    :new: Make Main work

--- Source/Main.js
+++ Source/Main.js
@@ -1,19 +1,31 @@
 "use strict"
 let FS = require('fs')
+let Helpers = require('./Helpers')
 class UniVoc{
-  static CompileFile(Path){
+  static ProcessFile(Path){
+    let Ext = Path.split('.')
+    let Opts = {Output: null}
+    Ext = Ext[ Ext.length - 1 ]
     return new Promise(function(Resolve){
-      let Ext = Path.split('.')
-      Ext = Ext[ Ext.length - 1 ]
-      if(!this.Plugins.has(Ext)){
+      if(!UniVoc.Plugins.has(Ext)){
         throw new Error(`Provided filetype '${Ext}' isn't recognized`)
       }
-      Resolve(FS.access(Path))
-    })
+      FS.access(Path, FS.R_OK, Resolve)
+    }).then(function(Access){
+        if(Access !== null) throw Access
+        let Stream = FS.createReadStream(Path)
+        for(let Plugin of UniVoc.Plugins.get(Ext)){
+          Stream = Stream.pipe(Plugin[1].Stream(Path, Opts))
+        }
+        return Stream
+      })
   }
 }
 UniVoc.Plugins = new Map
 require('./Plugins/JS').Register(UniVoc)
-UniVoc.CompileFile("/var/web/PublishSpace/dQuery/Source/Compile.js").then(function(Result){
+UniVoc.ProcessFile("/var/web/PublishSpace/dQuery/Source/Compile.js").then(function(Stream){
+  Stream.pipe(FS.createWriteStream("/tmp/test"))
   console.log(`Promise Result`)
+}, function(e){
+  console.log(e.stack)
 })
