2b159d65cdfc560b30a3f7f49a1b9c1357a2eb80
steelbrain <aneesiqbalbhatti@gmail.com>
2015-06-14 15:31:02 -0600

    :new: Add Babel Plugin

---
+++ Source/Plugins/Babel.js
@@ -0,0 +1,20 @@
+"use strict"
+let Base = require('../Plugin')
+let Babel
+let Path = require('path')
+class UniVocBabel extends Base{
+  static Process(UniVoc, Opts, Buffer){
+    Buffer = Buffer.toString("utf8")
+    if(!Opts.Transpile){
+      return Promise.resolve(Buffer)
+    }
+    Babel = Babel || require('babel')
+    return Promise.resolve(Babel.transform(Buffer, {
+      sourceRoot: Path.dirname(Opts.Path),
+      filename: Path.basename(Opts.path)
+    }).code)
+  }
+}
+UniVocBabel.Name = 'Babel'
+UniVocBabel.Ext = 'js'
+module.exports = UniVocBabel
